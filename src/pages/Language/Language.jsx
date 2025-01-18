2728ce5e7ad31ac6f46d0bc5a571096af1c2c605
harshalsakure18 <harshalsakure7758@gmail.com>
2025-01-18 17:35:44 +0530

    Language page

--- src/pages/Language/Language.jsx
+++ src/pages/Language/Language.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
-import Sidebar from '../../componants/Sidebar/Sidebar'
-import Navbar from '../../componants/Navbar/Navbar'
+import React from "react";
+import styles from "./Language.module.css";
 
-function Language() {
+const Language = () => {
   return (
-    <div>
-        
-      
-      <p>yfyyf</p>
+    <div className={styles.languagePage}>
+      {/* <h1 className={styles.title}>Languages</h1> */}
+      <div className={styles.cardsContainer}>
+        <div className={styles.card}>
+          <div className={styles.flag}>
+            <img src="./assets/England.jpg"></img>
+          </div>
+          <span className={styles.name}>English</span>
+        </div>
+        <div className={styles.card}>
+        <div className={styles.flag}>
+            <img src="./assets/Spain.jpg"></img>
+          </div>
+          <span className={styles.name}>Spanish</span>
+        </div>
+        <div className={styles.card}>
+        <div className={styles.flag}>
+            <img src="./assets/China.jpg"></img>
+          </div>
+          <span className={styles.name}>Mandarin</span>
+        </div>
+        <div className={styles.card}>
+        <div className={styles.flag}>
+            <img src="./assets/French.jpg"></img>
+          </div>
+          <span className={styles.name}>French</span>
+        </div>
+        <div className={styles.card}>
+        <div className={styles.flag}>
+            <img src="./assets/Japan.jpg"></img>
+          </div>
+          <span className={styles.name}>Japanese</span>
+        </div>
+        <div className={styles.card}>
+        <div className={styles.flag}>
+            <img src="./assets/Korea.jpg"></img>
+          </div>
+          <span className={styles.name}>Korean</span>
+        </div>
+        <div className={styles.card}>
+        <div className={styles.flag}>
+            <img src="./assets/German.jpg"></img>
+          </div>
+          <span className={styles.name}>German</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Language
+export default Language;
