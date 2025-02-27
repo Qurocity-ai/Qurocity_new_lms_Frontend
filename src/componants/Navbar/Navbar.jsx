f234b70a1ef9786a90d87c8584c78eb5a075c883
yogeshmasaistudent <yogeshkushawah93@gmail.com>
2025-02-27 15:56:14 +0530

    done

--- src/componants/Navbar/Navbar.jsx
+++ src/componants/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
 
-
 const Navbar = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
+
   return (
     <div className={styles.navbar}>
       <div className={styles.userProfile}>
-        <span>Shubham Paypare </span>
+        <span>{username ? username : "Guest"}</span> {/* Show username dynamically */}
         <img
-          src="/assets/profile.jpg" // Placeholder for profile picture
+          src="/assets/profile.jpg"
           alt="User Profile"
           className={styles.profileImage}
         />
