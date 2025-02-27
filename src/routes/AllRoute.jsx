c59592d08fa1f2dff0178cb08b4f9160e34121be
yogeshmasaistudent <yogeshkushawah93@gmail.com>
2025-02-27 15:28:00 +0530

    login

--- src/routes/AllRoute.jsx
+++ src/routes/AllRoute.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MultiStepQuiz from '../componants/Login/MultiStepQuiz';
 import Dashboard from '../componants/Dashboard/Dashboard';
 
 function AllRoute() {
   return (
     <Routes>
+      {/* Redirect the root path "/" to "/login" */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<MultiStepQuiz />} />
       <Route path="/dashboard/*" element={<Dashboard />} />
     </Routes>
